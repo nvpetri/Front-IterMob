36196ad363a5c074311d0a4bd1ddae11c4ec3268
Raica Martinez <martinezraica@gmail.com>
2024-09-05 16:46:02 -0300

    tela feita

--- src/Cadastro.js
+++ src/Cadastro.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import './Cadastro.css';
-
+import logo from './IterLogo.png';
 
 function Cadastro() {
   const [nome, setNome] = useState('');
@@ -8,75 +8,107 @@ function Cadastro() {
   const [telefone, setTelefone] = useState('');
   const [cpf, setCpf] = useState('');
   const [email, setEmail] = useState('');
+  const [senha, setSenha] = useState('');
+  const [confirmarSenha, setConfirmarSenha] = useState('');
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    // Lógica para processar os dados do formulário
+
     console.log('Nome:', nome);
     console.log('Sobrenome:', sobrenome);
     console.log('Telefone:', telefone);
     console.log('CPF:', cpf);
     console.log('Email:', email);
+    console.log('Senha:', senha);
+    console.log('Confirmar Senha:', confirmarSenha);
   };
 
   return (
-    <div className="cadastro-container">
-      <h2>Cadastro</h2>
-      <form onSubmit={handleSubmit}>
+    <div >
+      <header className="header">
+        <img src={logo} alt="Logo" className="logo" />
+      </header>
+      <div className="cadastro-container">
+
+        <h2>Cadastro</h2>
+        <form onSubmit={handleSubmit}>
         <div className="form-group">
-          <input
-            type="text"
-            id="nome"
-            placeholder="Nome" /* Placeholder no lugar do label */
-            value={nome}
-            onChange={(e) => setNome(e.target.value)}
-            required
-          />
-        </div>
-        <div className="form-group">
-          <input
-            type="text"
-            id="sobrenome"
-            placeholder="Sobrenome"
-            value={sobrenome}
-            onChange={(e) => setSobrenome(e.target.value)}
-            required
-          />
-        </div>
-        <div className="form-group">
-          <input
-            type="text"
-            id="telefone"
-            placeholder="Telefone"
-            value={telefone}
-            onChange={(e) => setTelefone(e.target.value)}
-            required
-          />
-        </div>
-        <div className="form-group">
-          <input
-            type="text"
-            id="cpf"
-            placeholder="CPF"
-            value={cpf}
-            onChange={(e) => setCpf(e.target.value)}
-            required
-          />
-        </div>
-        <div className="form-group">
-          <input
-            type="email"
-            id="email"
-            placeholder="Email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-          />
-        </div>
-        <button type="submit">Cadastrar</button>
-      </form>
+            <input
+              type="text"
+              id="cpf"
+              placeholder="CPF"
+              value={cpf}
+              onChange={(e) => setCpf(e.target.value)}
+              required
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="text"
+              id="nome"
+              placeholder="Nome"
+              value={nome}
+              onChange={(e) => setNome(e.target.value)}
+              required
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="text"
+              id="sobrenome"
+              placeholder="Sobrenome"
+              value={sobrenome}
+              onChange={(e) => setSobrenome(e.target.value)}
+              required
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="text"
+              id="telefone"
+              placeholder="Telefone"
+              value={telefone}
+              onChange={(e) => setTelefone(e.target.value)}
+              required
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="email"
+              id="email"
+              placeholder="Email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="password"
+              id="senha"
+              placeholder="Senha"
+              value={senha}
+              onChange={(e) => setSenha(e.target.value)}
+              required
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="password"
+              id="confirmarSenha"
+              placeholder="Confirmar Senha"
+              value={confirmarSenha}
+              onChange={(e) => setConfirmarSenha(e.target.value)}
+              required
+            />
+          </div>
+          <button type="submit">Cadastrar</button>
+        </form>
+      </div>
     </div>
   );
 }
 
 export default Cadastro;
+
+
