317bd86e950a2c5c06eb638309dcdf4b76b9ecf1
Carolina Neponucena <123747829+carolneponucenaa@users.noreply.github.com>
2024-10-28 20:29:12 -0300

    sobre nos (#4)
    * sobre nos
    
    * quase ´pronto

--- src/App.js
+++ src/App.js
@@ -1,19 +1,11 @@
 import React from 'react';
-<<<<<<< HEAD
-import Cadastro from './Cadastro';  
-=======
-import Login from './Telahome'; 
->>>>>>> 06618ce1565a05650336a6dde402e158aeb16f2a
+import Login from './Download'; 
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-<<<<<<< HEAD
-      <Cadastro />  {}
-=======
+    <div className="IterMob">
       <Login />  {}
->>>>>>> 06618ce1565a05650336a6dde402e158aeb16f2a
     </div>
   );
 }
@@ -22,7 +14,4 @@ export default App;
 
 
 
-<<<<<<< HEAD
-=======
 
->>>>>>> 06618ce1565a05650336a6dde402e158aeb16f2a
