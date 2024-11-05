d1efb386983affc8355328b816278a1243965204
RAICA RODRIGUES MARTINEZ CASTRO <23111763@CFP127ALUNO.LOCAL>
2024-11-05 14:57:28 -0300

    Quase Finalizado

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Login from './Download'; 
+import Login from './FACResponse'; 
 import './App.css';
 
 function App() {
--- src/FACResponse.js
+++ src/FACResponse.js
@@ -3,7 +3,6 @@ import './FACResponse.css';
 import logo from './IterLogo.png'
 import PerfilVirginia from './PerfilVirginia.png'
 
-
 function FACResponse() {
   const [response, setResponse] = useState('');
 
@@ -13,7 +12,6 @@ function FACResponse() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    
     console.log('Resposta enviada:', response);
     setResponse('');
   };
@@ -21,32 +19,33 @@ function FACResponse() {
   return (
     <div className="fac-response-container">
       <header>
-        <a href="/"><i className="back-arrow">&#8592;</i></a>
         <img src={logo} alt="Logo" className="logo" />
       </header>
 
-      <div className="feedback-card">
-        <div className="user-info">
-        <img src={PerfilVirginia} alt="Perfil de Virgínia Fonseca" className="user-photo"/>
-          <div>
-            <p className="user-name">Virginia Fonseca</p>
-            <p className="user-code">T246</p>
+      <div className="feedback-container">
+        <div className="feedback-card">
+          <div className="user-info">
+            <img src={PerfilVirginia} alt="Perfil de Virgínia Fonseca" className="user-photo"/>
+            <div>
+              <p className="user-name">Virginia Fonseca</p>
+              <p className="user-code">T246</p>
+            </div>
           </div>
+          <p className="feedback-text">Falta de limpeza no ônibus e a janela estava emperrada.</p>
         </div>
-        <p className="feedback-text">Falta de limpeza no ônibus e a janela estava emperrada.</p>
-      </div>
 
-      <form className="response-form" onSubmit={handleSubmit}>
-        <label htmlFor="response">RESPONDER</label>
-        <textarea
-          id="response"
-          placeholder="Escreva aqui seu comentário..."
-          value={response}
-          onChange={handleInputChange}
-          required
-        ></textarea>
-        <button type="submit" className="send-button">ENVIAR</button>
-      </form>
+        <form className="response-form" onSubmit={handleSubmit}>
+          <label htmlFor="response">RESPONDER</label>
+          <textarea
+            id="response"
+            placeholder="Escreva aqui seu comentário..."
+            value={response}
+            onChange={handleInputChange}
+            required
+          ></textarea>
+          <button type="submit" className="send-button">ENVIAR</button>
+        </form>
+      </div>
 
       <footer>
         <p>Copyright 2024 © IterMob</p>
