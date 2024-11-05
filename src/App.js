adea5c050a1d583ae44d06bca2c9cc391a687206
RAICA RODRIGUES MARTINEZ CASTRO <23111763@CFP127ALUNO.LOCAL>
2024-11-05 16:11:39 -0300

    Quase Finalizado

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Login from './Telahome'; 
+import Login from './Sobre'; 
 import './App.css';
 
 function App() {
--- src/Login.js
+++ src/Login.js
@@ -22,6 +22,7 @@ function Login() {
       </div>
 
       <div className="login-container">
+      <h2>ACESSO ADMINISTRADOR</h2>
         <form onSubmit={handleSubmit}>
           <div className="form-group">
             <input
